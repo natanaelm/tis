8714a58f4b10e0317e38ce1f960c9d89c146c1ef
natanaelm <nmp.c.email@gmail.com>
2023-03-17 13:36:41 -0300

    mobile unpacked

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+// Projeto Tis
+
+/* mobPrototype = {
+  objects: {
+    [objname, x, y]
+  },
+  frames: {
+    [objname, x, y]
+  }
+}  */
+
+function drawMap(map, texture) {
+  for(let mx in map) {
+    for(let my in mx) {
+      
+    }
+  }
+}
+
+function main(stage, display){
+  stage.viewbox(360,360)
+
+}
+
+document.onload = () => {
+  Stage(main)
+  Stage(mobile)
+}
